92c2881ca314a994e5ef13ec2cc1b86a1d274928
Lê Hữu Tấn <huutanctct@gmail.com>
2023-12-12 17:09:09 +0700

    Update Day12.java

--- Day12.java
+++ Day12.java
@@ -1,86 +1,77 @@
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
-public class Day12 {
+public class Test {
+
+    private static final int COPIES = 5; // Number of times to repeat the pattern and groups
+    private static final Map<String, Long> memo = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
-        long totalArrangements = 0;
-
-        try (var scanner = new Scanner(new FileReader("file.txt"))) {
-            while (scanner.hasNext()) {
-                totalArrangements += countArrangements(scanner.nextLine());
-            }
-        }
-
+        var totalArrangements = Files.readAllLines(Paths.get("file.txt"))
+            .stream()
+            .map(Test::countArrangements)
+            .reduce(0L, Long::sum);
         System.err.println("Total number of arrangements: " + totalArrangements);
     }
 
     private static long countArrangements(String line) {
-        String[] parts = line.split(" ");
-        char[] pattern = parts[0].toCharArray();
-        int[] groups = Arrays.stream(parts[1].split(",")).mapToInt(Integer::parseInt).toArray();
+        var parts = line.split(" ");
+        var pattern = String.join("?", Collections.nCopies(COPIES, parts[0]));
+        int[] groups = Arrays.stream(String.join(",", Collections.nCopies(COPIES, parts[1])).split(","))
+            .mapToInt(Integer::parseInt)
+            .toArray();
 
-        // List to hold all the possible patterns
-        List<String> possiblePatterns = new ArrayList<>();
-        generatePatterns(possiblePatterns, pattern, 0);
-
-        // Count only the patterns that match the group sizes
-        return possiblePatterns.stream().filter(p -> isValidPattern(p, groups)).count();
+        memo.clear();
+        return countArrangements(pattern, groups, 0, 0, 0, false);
     }
 
-    private static void generatePatterns(List<String> possiblePatterns, char[] pattern, int index) {
-        if (index == pattern.length) {
-            // Reached the end of the pattern, add it to the list
-            possiblePatterns.add(new String(pattern));
-            return;
+    private static long countArrangements(String pattern, int[] groups, int index, int groupIndex, int damagedCount, boolean prevDamaged) {
+        // Check if the result is already computed.
+        String memoKey = getMemoKey(index, groupIndex, damagedCount, prevDamaged);
+        if (memo.containsKey(memoKey)) {
+            return memo.get(memoKey);
         }
 
-        if (pattern[index] == '?') {
-            // Try with a damaged spring
-            pattern[index] = '#';
-            generatePatterns(possiblePatterns, pattern, index + 1);
-
-            // Try with an operational spring
-            pattern[index] = '.';
-            generatePatterns(possiblePatterns, pattern, index + 1);
-
-            // Reset to unknown for backtracking
-            pattern[index] = '?';
-        } else {
-            // If not unknown, just continue to the next character
-            generatePatterns(possiblePatterns, pattern, index + 1);
-        }
-    }
-
-    private static boolean isValidPattern(String pattern, int[] groups) {
-        int groupIndex = 0;
-        int count = 0;
-
-        for (int i = 0; i < pattern.length(); i++) {
-            if (pattern.charAt(i) == '#') {
-                count++;
-            } else {
-                if (count > 0) {
-                    if (groupIndex >= groups.length || groups[groupIndex] != count) {
-                        return false;
-                    }
-                    groupIndex++;
+        // If all groups are done, we just need to fill the remaining with operational springs
+        if (groupIndex == groups.length) {
+            for (int i = index; i < pattern.length(); i++) {
+                if (pattern.charAt(i) == '#') {
+                    return 0;
                 }
-                count = 0;
+            }
+            return 1;
+        }
+
+        // If all characters are processed, check if we have a valid configuration
+        if (index == pattern.length()) {
+            // All groups should be accounted for and the last group should be completed
+            return (groupIndex == groups.length - 1 && damagedCount == groups[groupIndex]) ? 1 : 0;
+        }
+
+        char current = pattern.charAt(index);
+        long count = 0;
+
+        // When the current character is unknown or damaged, and we have not exceeded the group size
+        if ((current == '?' || current == '#') && damagedCount < groups[groupIndex]) {
+            count += countArrangements(pattern, groups, index + 1, groupIndex, damagedCount + 1, true);
+        }
+
+        // When the current character is unknown or operational, we may need to move to the next group
+        if (current == '?' || current == '.') {
+            // If the previous was damaged and we completed a group, or if the previous was not damaged
+            if (!prevDamaged || damagedCount == groups[groupIndex]) {
+                count += countArrangements(pattern, groups, index + 1, prevDamaged && damagedCount == groups[groupIndex] ? groupIndex + 1 : groupIndex, 0, false);
             }
         }
 
-        if (count > 0) {
-            if (groupIndex >= groups.length || groups[groupIndex] != count) {
-                return false;
-            }
-            groupIndex++;
-        }
-
-        return groupIndex == groups.length;
+        // Save the computed result.
+        memo.put(memoKey, count);
+        return count;
     }
-    
+
+    private static String getMemoKey(int index, int groupIndex, int damagedCount, boolean prevDamaged) {
+        return index + "-" + groupIndex + "-" + damagedCount + "-" + prevDamaged;
+    }
+
 }
