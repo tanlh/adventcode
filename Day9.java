51f7dfc7c31b9dd6bd22e02a4e71165cb15434a4
Lê Hữu Tấn <huutanctct@gmail.com>
2023-12-19 00:24:19 +0700

    Update Day9.java

--- Day9.java
+++ Day9.java
@@ -1,46 +1,35 @@
-import org.apache.commons.lang3.StringUtils;
+package com.alibaba.logistics.station;
 
-import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day9 {
 
     public static void main(String[] args) {
-        List<List<Integer>> histories = new ArrayList<>();
-        try (var scanner = new Scanner(new File("file.txt"))) {
-            while (scanner.hasNextLine()) {
-                histories.add(lineToValues(scanner.nextLine()));
-            }
-        } catch (Exception ignored) {
-        }
+        var histories = Util.readFileToLines().stream()
+            .map(line -> Util.parseLineToLongs(line, " "))
+            .collect(Collectors.toList());
 
-        int sumNext = histories.stream()
-            .map(Test::findNextValue)
-            .reduce(0, Integer::sum);
+        var sumNext = histories.stream()
+            .map(Day9::findNextValue)
+            .reduce(0L, Long::sum);
         System.out.println("Sum next: " + sumNext);
 
-        int sumBack = histories.stream()
+        var sumBack = histories.stream()
             .peek(Collections::reverse)
-            .map(Test::findNextValue)
-            .reduce(0, Integer::sum);
+            .map(Day9::findNextValue)
+            .reduce(0L, Long::sum);
         System.out.println("Sum back: " + sumBack);
     }
 
-    private static List<Integer> lineToValues(String line) {
-        return Arrays.stream(line.split(" "))
-            .map(String::trim)
-            .filter(StringUtils::isNotBlank)
-            .map(Integer::parseInt)
-            .collect(Collectors.toList());
-    }
-
-    public static int findNextValue(List<Integer> sequence) {
+    private static long findNextValue(List<Long> sequence) {
         var lastNumber = sequence.get(sequence.size() - 1);
-        List<Integer> lastNumbers = new ArrayList<>();
+        List<Long> lastNumbers = new ArrayList<>();
 
         while (anyNonZero(sequence)) {
-            List<Integer> nextSequence = new ArrayList<>();
+            List<Long> nextSequence = new ArrayList<>();
             for (int i = 0; i < sequence.size() - 1; i++) {
                 nextSequence.add(sequence.get(i + 1) - sequence.get(i));
             }
@@ -48,10 +37,10 @@ public class Day9 {
             sequence = nextSequence;
         }
 
-        return lastNumber + lastNumbers.stream().reduce(0, Integer::sum);
+        return lastNumber + lastNumbers.stream().reduce(0L, Long::sum);
     }
 
-    private static boolean anyNonZero(List<Integer> sequence) {
+    private static boolean anyNonZero(List<Long> sequence) {
         return sequence.stream().anyMatch(i -> i != 0);
     }
 
