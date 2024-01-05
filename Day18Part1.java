9fc3f9c01ac624008a6f29e19ad49fe16f02e686
Lê Hữu Tấn <huutanctct@gmail.com>
2024-01-05 17:08:56 +0700

    Create Day18Part1.java

---
+++ Day18Part1.java
@@ -0,0 +1,82 @@
+package com.alibaba.logistics.station;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.*;
+
+public class Day18Part1 {
+
+    static int[][] DIRECTIONS = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+    static int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        var points = buildBoundary(Util.readFileToLines());
+        var result = calculateArea(points);
+        System.out.println("Result: " + result);
+    }
+
+    private static Set<Point> buildBoundary(List<String> digPlan) {
+        Set<Point> points = new HashSet<>();
+        int x = 0, y = 0;
+        points.add(new Point(x, y));
+
+        for (var step : digPlan) {
+            var splitStep = step.split(" ");
+            var direction = splitStep[0].charAt(0);
+            var distance = Integer.parseInt(splitStep[1]);
+
+            for (int i = 0; i < distance; i++) {
+                x += direction == 'L' ? -1 : direction == 'R' ? 1 : 0;
+                y += direction == 'U' ? -1 : direction == 'D' ? 1 : 0;
+                points.add(new Point(x, y));
+
+                minX = Math.min(x, minX);
+                maxX = Math.max(x, maxX);
+                minY = Math.min(y, minY);
+                maxY = Math.max(y, maxY);
+            }
+        }
+
+        return points;
+    }
+
+    private static long calculateArea(Set<Point> points) {
+        minX--;
+        minY--;
+        maxX++;
+        maxY++;
+
+        Point startPoint = new Point(minX, minY);
+        Set<Point> outsidePoints = new HashSet<>();
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(startPoint);
+        outsidePoints.add(startPoint);
+
+        while (!queue.isEmpty()) {
+            var point = queue.poll();
+
+            for (int[] direction : DIRECTIONS) {
+                var newX = point.x + direction[0];
+                var newY = point.y + direction[1];
+                var newPoint = new Point(newX, newY);
+
+                if (newX >= minX && newX <= maxX && newY >= minY && newY <= maxY
+                    && !points.contains(newPoint) && outsidePoints.add(newPoint)) {
+                    queue.add(newPoint);
+                }
+            }
+        }
+
+        long boundaryArea = (long) (Math.abs(minX - maxX) + 1) * (Math.abs(minY - maxY) + 1);
+        return boundaryArea - outsidePoints.size();
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Point {
+        int x, y;
+    }
+
+}
