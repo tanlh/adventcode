806a7b469af1379a892aad3d8485f7a77b7e0f66
Lê Hữu Tấn <huutanctct@gmail.com>
2024-01-05 18:47:16 +0700

    Update and rename Day18Part1.java to Day18.java

---
+++ Day18.java
@@ -0,0 +1,88 @@
+package com.alibaba.logistics.station;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        var instructions = parseInstructions();
+        var vertices = parseVertices(instructions);
+        var area = IntStream.range(0, vertices.size() - 1)
+            .mapToDouble(i -> {
+                var p1 = vertices.get(i);
+                var p2 = vertices.get(i + 1);
+                return (p1.x * p2.y) - (p2.x * p1.y);
+            })
+            .sum();
+        var perimeter = instructions.stream()
+            .map(Instruction::getDistance)
+            .reduce(0D, Double::sum);
+
+        /**
+         * Pick's theorem
+         */
+        var result = Math.abs(area) / 2 + (perimeter / 2) + 1;
+        System.err.println("Result: " + (long) result);
+    }
+
+    private static List<Instruction> parseInstructions() {
+        return Util.readFileToLines().stream()
+            .map(line -> {
+                var parts = line.split(" ");
+                // part 1
+//                var direction = parts[0].charAt(0);
+//                var distance = Integer.parseInt(parts[1]);
+                // part 2
+                var direction = parts[2].charAt(7);
+                var distance = (double) Integer.parseInt(parts[2].substring(2, 7), 16);
+                return new Instruction(direction, distance);
+            })
+            .collect(Collectors.toList());
+    }
+
+    private static List<Point> parseVertices(List<Instruction> instructions) {
+        int x = 0, y = 0;
+        List<Point> vertices = new ArrayList<>();
+        vertices.add(new Point(x, y));
+
+        for (var instr : instructions) {
+            var dx = instr.direction == 'L' ? -1 : instr.direction == 'R' ? 1 : 0;
+            var dy = instr.direction == 'U' ? -1 : instr.direction == 'D' ? 1 : 0;
+            x += dx * instr.distance;
+            y += dy * instr.distance;
+            vertices.add(new Point(x, y));
+        }
+
+        return vertices;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Point {
+        double x, y;
+    }
+
+    @Data
+    static class Instruction {
+        char direction;
+        double distance;
+
+        public Instruction(char direction, double distance) {
+            switch (direction) {
+                case '0': this.direction = 'R'; break;
+                case '1': this.direction = 'D'; break;
+                case '2': this.direction = 'L'; break;
+                case '3': this.direction = 'U'; break;
+                default: this.direction = direction;
+            }
+            this.distance = distance;
+        }
+    }
+
+}
