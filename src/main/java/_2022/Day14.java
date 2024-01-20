3cd98cc236729074ac8870480579dc2f9580e175
tan.lehuu <tan.lehuu@lazada.com>
2024-01-21 03:42:19 +0700

    Utilize point class for day 14 2022

--- src/main/java/_2022/Day14.java
+++ src/main/java/_2022/Day14.java
@@ -1,7 +1,6 @@
 package _2022;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import util.Point;
 import util.Util;
 
 import java.util.HashSet;
@@ -10,19 +9,7 @@ import java.util.Set;
 public class Day14 {
 
     static long minX = Long.MAX_VALUE, maxX = Long.MIN_VALUE, maxY = Long.MIN_VALUE;
-    static Sand source = new Sand(500, 0);
-
-    @Data
-    @AllArgsConstructor
-    static class Sand {
-        int x, y;
-
-        public Sand(String point) {
-            var coordinate = Util.parseLine(point, ",", Integer::parseInt);
-            this.x = coordinate.get(0);
-            this.y = coordinate.get(1);
-        }
-    }
+    static Point source = new Point(500, 0);
 
     public static void main(String[] args) {
         var border = buildBorder();
@@ -30,21 +17,21 @@ public class Day14 {
         System.err.println("Part 2: " + simulateSand(border, true));
     }
 
-    private static int simulateSand(Set<Sand> border, boolean hasFloor) {
+    private static int simulateSand(Set<Point> border, boolean hasFloor) {
         var settledSand = new HashSet<>(border);
         var count = 0;
 
         while (!settledSand.contains(source)) {
-            var sand = new Sand(source.x, source.y);
+            var sand = new Point(source.x, source.y);
             while (true) {
                 if (hasFloor && sand.y == maxY + 1) break;
                 if (!hasFloor && (sand.x < minX || sand.x > maxX || sand.y > maxY)) return count;
 
-                if (!settledSand.contains(new Sand(sand.x, sand.y + 1))) {
+                if (!settledSand.contains(new Point(sand.x, sand.y + 1))) {
                     // ignored
-                } else if (!settledSand.contains(new Sand(sand.x - 1, sand.y + 1))) {
+                } else if (!settledSand.contains(new Point(sand.x - 1, sand.y + 1))) {
                     sand.x--;
-                } else if (!settledSand.contains(new Sand(sand.x + 1, sand.y + 1))) {
+                } else if (!settledSand.contains(new Point(sand.x + 1, sand.y + 1))) {
                     sand.x++;
                 } else {
                     break;
@@ -59,21 +46,21 @@ public class Day14 {
         return count;
     }
 
-    private static Set<Sand> buildBorder() {
-        Set<Sand> borders = new HashSet<>();
+    private static Set<Point> buildBorder() {
+        Set<Point> borders = new HashSet<>();
 
         for (var line : Util.readFileToLines()) {
             var points = line.split(" -> ");
             for (var i = 0; i < points.length - 1; i++) {
-                var p1 = new Sand(points[i]);
-                var p2 = new Sand(points[i + 1]);
+                var p1 = new Point(Util.parseLine(points[i], ",", Long::parseLong));
+                var p2 = new Point(Util.parseLine(points[i + 1], ",", Long::parseLong));
                 minX = Math.min(minX, Math.min(p1.x, p2.x));
                 maxX = Math.max(maxX, Math.max(p1.x, p2.x));
                 maxY = Math.max(maxY, Math.max(p1.y, p2.y));
 
                 for (var x = Math.min(p1.x, p2.x); x <= Math.max(p1.x, p2.x); x++) {
                     for (var y = Math.min(p1.y, p2.y); y <= Math.max(p1.y, p2.y); y++) {
-                        borders.add(new Sand(x, y));
+                        borders.add(new Point(x, y));
                     }
                 }
             }
--- src/main/java/util/Point.java
+++ src/main/java/util/Point.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Point {
 
     public long x, y;
+
+    public Point(List<Long> coordinate) {
+        this.x = coordinate.get(0);
+        this.y = coordinate.get(1);
+    }
 
 }
