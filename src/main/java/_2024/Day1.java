ce8dda6fa012483ba1fb46d5412081f481319510
Le Huu Tan <huutanctct@gmail.com>
2024-12-02 13:00:18 +0700

    Day 2 2024

--- src/main/java/_2024/Day1.java
+++ src/main/java/_2024/Day1.java
@@ -5,7 +5,6 @@ import util.Util;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
---
+++ src/main/java/_2024/Day2.java
@@ -0,0 +1,44 @@
+package _2024;
+
+import util.Util;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        var reports = Util.readFileToLines().stream()
+            .map(rp -> Util.parseLine(rp, "\\s+", Integer::parseInt))
+            .toList();
+
+        var part1 = reports.stream().filter(Day2::isSafe1).count();
+        System.err.println("Part 1: " + part1);
+
+        var part2 = reports.stream().filter(Day2::isSafe2).count();
+        System.err.println("Part 2: " + part2);
+    }
+
+    private static boolean isSafe2(List<Integer> levels) {
+        return isSafe1(levels) ||
+            IntStream.range(0, levels.size())
+                .anyMatch(i -> isSafe1(
+                    IntStream.range(0, levels.size())
+                        .filter(j -> j != i)
+                        .mapToObj(levels::get)
+                        .toList()
+                ));
+    }
+
+    private static boolean isSafe1(List<Integer> levels) {
+        var direction = Math.signum(levels.get(0) - levels.get(1));
+        return IntStream.range(0, levels.size() - 1)
+            .allMatch(i -> {
+                var diff = levels.get(i) - levels.get(i + 1);
+                return Math.signum(diff) == direction &&
+                    Math.abs(diff) >= 1 &&
+                    Math.abs(diff) <= 3;
+            });
+    }
+
+}
